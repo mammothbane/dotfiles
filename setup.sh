73c678fe6a37c5f85193b072fc5805b0ffd7e5c9
Nathan Perry <nathan@meta.sc>
2015-11-30 23:51:16 -0500

    non-destructive
    commenting as well

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 odir=$HOME
 script_path=$(realpath $0)
 script_dir=$(dirname $script_path)
+oldDir=$HOME/.old
 
 set -e
 
@@ -11,6 +12,8 @@ if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+mkdir -p $oldDir # create a directory to house the old contents of the home directory
+
 for file in $script_dir/*; do
     # don't copy this script over
     if [[ $file == $script_path ]]; then
@@ -18,18 +21,16 @@ for file in $script_dir/*; do
     fi
 
     dest=$odir/.$(basename $file)
-        
+
+    # if the file exists and is a symlink, remove it so we can re-link it
     if [ -e $dest ]; then
 	if [ -L $dest ]; then
 	    rm $dest
-	else
-	    if [ ! -n $oldDir ]; then
-		oldDir=$HOME/.old
-	    fi
-
-	    mv $dest $oldDir/$(basename $dest)
+	else # otherwise, move the old file into the .old directory with a timestamp
+	    mv $dest $oldDir/$(basename $dest)-$(date +%s)
 	fi
     fi
 
-    ln -sf $file $odir/.$(basename $file)
+    # link the file where it should be 
+    ln -sf $file $dest
 done
