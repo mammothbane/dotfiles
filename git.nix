e2310855469f39c6d7837b8e0d284c5c8a60c441
Nathan Perry <np@nathanperry.dev>
2020-12-31 01:41:38 -0500

    script to enable/disable signing in git

--- git.nix
+++ git.nix
@@ -1,6 +1,31 @@
 { pkgs, ... }:
 
-{
+let
+  setsign = pkgs.writeScript "setsign" ''
+    #!${pkgs.python3}/bin/python3
+
+    from configparser import ConfigParser
+    from pathlib import Path
+    import sys
+
+    CONFIG_PATH = Path.home() / '.gitconfig_local'
+
+    parser = ConfigParser()
+    parser.read(CONFIG_PATH)
+
+    if not parser.has_section('commit'):
+        parser.add_section('commit')
+
+    if parser['commit'].get('gpgsign') == sys.argv[1]:
+        sys.exit(0)
+
+    parser['commit']['gpgsign'] = sys.argv[1]
+
+    with open(CONFIG_PATH, 'w') as f:
+        parser.write(f)
+  '';
+
+in {
   programs.git = {
     enable = true;
     userName = "Nathan Perry";
@@ -33,6 +58,9 @@
       graph = "log --graph  --decorate --all --date-order --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
 
       yolo = "!git commit -m \"$(${pkgs.curl}/bin/curl -s whatthecommit.com/index.txt)\"";
+
+      enable-signing = "!${setsign} true";
+      disable-signing = "!${setsign} false";
     };
 
     signing = {
