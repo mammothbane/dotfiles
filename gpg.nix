102f4480aede1db5a3cafa300bb0f8fcb83be88f
Nathan Perry <np@nathanperry.dev>
2021-01-01 22:46:33 -0500

    mkdefault

--- gpg.nix
+++ gpg.nix
@@ -2,7 +2,7 @@
 {
 
   home.sessionVariables = lib.optionalAttrs config.services.gpg-agent.enableSshSupport {
-    SSH_AUTH_SOCK = "\${SSH_AUTH_SOCK:-$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)}";
+    SSH_AUTH_SOCK = lib.mkDefault "\${SSH_AUTH_SOCK:-$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)}";
   };
 
   services.gpg-agent = {
