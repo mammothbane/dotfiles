91fa99eaf985ac7c8052ce2f28ba7355ea240b79
Nathan Perry <np@nathanperry.dev>
2021-01-03 09:26:39 -0500

    vim: create directories for file on save

--- vim.nix
+++ vim.nix
@@ -112,6 +112,20 @@
       "    \ 'smart_case': v:true,
       "    \})
 
+      function s:MkNonExDir(file, buf)
+        if empty(getbufvar(a:buf, '&buftype')) && a:file!~#'\v^\w+\:\/'
+          let dir=fnamemodify(a:file, ':h')
+          if !isdirectory(dir)
+            call mkdir(dir, 'p')
+          endif
+        endif
+      endfunction
+
+      augroup BWCCreateDir
+        autocmd!
+        autocmd BufWritePre * :call s:MkNonExDir(expand('<afile>'), +expand('<abuf>'))
+      augroup END
+
       set shortmess+=c
 
       let g:mix_format_on_save = 1
