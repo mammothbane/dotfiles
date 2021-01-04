9012f63d5c298de6eba4036efa6d428b021b5338
Nathan Perry <np@nathanperry.dev>
2021-01-04 06:50:24 -0500

    fix direnv version by just using a new nixpkgs (buildGoModules is not overridable)

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-util.url = "github:numtide/flake-utils";
 
+    newpkgs.url = "github:nixos/nixpkgs/master";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -79,6 +81,11 @@
 
   outputs = { self, nixpkgs, flake-util, ... } @ inputs:
     let
+      newpkgs = import inputs.newpkgs {
+        system = "x86_64-linux";
+        config = import ./nixpkgs-config.nix;
+      };
+
       vim-overlay = (import ./vim-plugins.nix {
         inherit (inputs) darcula ncm2-alchemist ncm2-go ncm2-pyclang ncm2-racer ncm2-tern nvim-typescript ncm2-vim vim-mix-format;
       });
@@ -89,9 +96,7 @@
           neuron = import inputs.neuron {};
           nix-bisect = inputs.nix-bisect;
 
-          direnv = super.direnv.overrideAttrs (old: {
-            version = "2.27.0";
-          });
+          inherit (newpkgs) direnv;
         } //
         (import ./pkgs {
           inherit (self) callPackage;
@@ -129,7 +134,7 @@
           inherit pkgs;
 
           configuration = {
-            nixpkgs.overlays = self.overlays;
+            nixpkgs.overlays = overlays;
 
             imports = [
               ./main.nix
@@ -166,59 +171,6 @@
       lib = {
         inherit mkActivator;
       };
-
-      nixosModule = ({ config, pkgs, lib, ... }: {
-        environment.systemPackages = [
-          (let
-            users = config.users.users;
-
-            extractName = attrName:
-            let maybeName = users.${attrName}.name;
-            in if maybeName == null then attrName else maybeName;
-
-            isHomeManageable = attrName: let
-              home = users.${attrName}.home;
-              pref = lib.flip lib.strings.hasPrefix home;
-            in
-            home != null &&
-            (
-              (extractName attrName) == "root" ||
-              pref "/home"
-              );
-
-              manageableUsers = builtins.filter isHomeManageable (builtins.attrNames users);
-
-              userDb = pkgs.writeText "users.json" (builtins.toJSON (
-                builtins.foldl'
-                (acc: x:
-                  let
-                    username = extractName x;
-                    homeDirectory = users.${x}.home;
-                    activator = mkActivator { inherit username homeDirectory; };
-                  in acc // {
-                    ${username} = "${activator}/activate";
-                  })
-                {}
-                manageableUsers
-                ));
-
-          in pkgs.writeShellScriptBin "hm-system" ''
-                set -uo pipefail
-
-                me=$(id -u -n)
-
-                activator=$(<"${userDb}" "${pkgs.jq}/bin/jq" -re ".\"$me\"")
-
-                if [ $? -ne 0 ]; then
-                  echo "this user is not capable of using $(basename ''${BASH_SOURCE[0]}) (no home directory in nixos)" >&2
-
-                  exit 1
-                fi
-
-                exec "$activator"
-          '')
-        ];
-      });
 
       packages.x86_64-linux = packages;
       apps.x86_64-linux = apps;
